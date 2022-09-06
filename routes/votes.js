e0b55377e854b838833fe8ab7b9174bc66ae1d17
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2022-09-06 20:54:36 +0200

    watchlist route

--- routes/votes.js
+++ routes/votes.js
@@ -9,15 +9,21 @@ const {isAuthenticated} = require('../middlewares/jwt')
 // @desc    Posts add movie to array watchlist
 // @route   POST /votes/:movieId/watchLater
 // @access  User
-router.post('/:movieId/addToWatchlist', isAuthenticated, async (req,res,next)=>{
-const user = req.payload;
+router.put('/:movieId/addToWatchlist', isAuthenticated, async (req,res,next)=>{
+const userId = req.payload._id
+const user = req.payload
 const{movieId}= req.params;
-console.log(user.watchList)
+console.log(user._id)
+//console.log('MOVIE ID:!', movieId)
+//console.log(user.watchList)
 try {
-    const foundMovieDb = await Movie.findById(movieId)
-   // console.log(foundMovieDb)
-    
-    //res.status(201).json({data: movieAdded})
+    const foundMovie = await Movie.findById(movieId)
+    const newWatchList = user.watchList.push(foundMovie)
+    //console.log('this is new watchlist:',newWatchList)
+    const updatedUser = await User.findByIdAndUpdate(user, {watchList: newWatchList}, {new: true});
+    req.payload = updatedUser;
+    //console.log('this is updated user:', updatedUser)
+    res.status(202).json({data: updatedUser})
 } catch (error) {
     next(error)
 }
