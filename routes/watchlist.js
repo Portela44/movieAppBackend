6e9077e7f5972f200bba5a3e023af8c3fd83494c
Pau Mesonero <paumesonero@Paus-Mac-mini.local>
2022-09-07 23:26:32 +0200

    syntax stuff

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -1,47 +1,54 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Movie = require("../models/Movie");
+const Vote = require("../models/Vote");
+const User = require("../models/User");
 const WatchList = require("../models/WatchList");
 const {isAuthenticated} = require('../middlewares/jwt');
 
 // @desc    Posts add movie to watchlist
-// @route   POST /watchList/:movieId/add
+// @route   POST /watchList/:movieId/addToWatchlist
 // @access  User
-router.post('/:movieId/add', isAuthenticated, async (req, res, next) => {
+
+router.post('/:movieId/addToWatchlist', isAuthenticated, async (req,res,next)=>{
     const userId = req.payload._id;
     const {movieId} = req.params;
     try {
-        const addedMovie = await WatchList.create({movieId, userId:userId})
-        res.status(201).json({data: addedMovie})
+        const addedMovie = await WatchList.create({movieId, userId:userId});
+        res.status(201).json({data: addedMovie});
     } catch (error) {
-        next(error);
+        next(error)
     }
-});
+})
 
 // @desc    Removes a movie from whatchlist
-// @route   POST /watchList/:movieId/remove
+// @route   DELETE /watchList/:movieId/remove
 // @access  User
-router.delete('/:movieId/remove', isAuthenticated, async (req, res, next) => {
+
+router.delete('/:movieId/remove', isAuthenticated, async (req,res,next)=>{
     const userId = req.payload._id;
     const {movieId} = req.params;
     try {
-        const removedMovie = await WatchList.findOneAndDelete({userId:userId, movieId});
-        res.status(202).json({data: removedMovie});
+        const removeMovie = await WatchList.findOneAndDelete({userId:userId, movieId});
+        res.status(202).json({data: removeMovie});
     } catch (error) {
-        next(error);
+        next(error)
     }
-});
-
+})
 // @desc    Show the user the movies in watchlist
-// @route   GET /watchList
+// @route   GET /watchList/myWatchList
 // @access  User
-router.get('/', isAuthenticated, async (req, res, next) => {
-    const userId = req.payload._id;
+
+router.get('/myWatchList', isAuthenticated, async (req,res,next)=>{
+    const userId = req.payload._id
     try {
         const moviesFromDb = await WatchList.find({userId: userId});
         res.status(200).json({data: moviesFromDb});
     } catch (error) {
         next(error)
     }
-});
+})
+
 
 module.exports = router;
