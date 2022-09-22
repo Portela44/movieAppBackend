5cee6910177d1aad5db07fcd079866a9c4f8dbf4
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2022-09-22 20:06:58 +0200

    reviews, user descomented

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const errorHandler = require('./middlewares/errorHandler');
 // Routers require
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
-// const userRouter = require('./routes/user');
+const userRouter = require('./routes/user');
 const moviesRouter = require('./routes/movies');
-// const reviewsRouter = require('./routes/reviews');
+const reviewsRouter = require('./routes/reviews');
 // const votesRouter = require('./routes/votes');
 // const watchListRouter = require('./routes/watchList');
 const reviewLikeRouter = require('./routes/reviewLike');
@@ -35,9 +35,9 @@ app.use(express.urlencoded({ extended: false }));
 // routes intro
 app.use('/', indexRouter);
 app.use('/api/v1/auth', authRouter);
-// app.use('/api/v1/user', userRouter);
+app.use('/api/v1/user', userRouter);
 app.use('/api/v1/movies', moviesRouter);
-// app.use('/api/v1/reviews', reviewsRouter)
+app.use('/api/v1/reviews', reviewsRouter)
 // app.use('/api/v1/votes', votesRouter);
 // app.use('/api/v1/watchList', watchListRouter);
 app.use('/api/v1/reviewLike', reviewLikeRouter);
--- routes/user.js
+++ routes/user.js
@@ -1,107 +1,107 @@
-// const express = require('express');
-// const router = express.Router();
-// const {isAuthenticated, isAdmin} = require('../middlewares/jwt')
-// const User = require("../models/User");
-// const fileUploader = require('../config/cloudinary.config');
-// const ErrorResponse = require('../utils/error');
+const express = require('express');
+const router = express.Router();
+const {isAuthenticated, isAdmin} = require('../middlewares/jwt')
+const User = require("../models/User");
+const fileUploader = require('../config/cloudinary.config');
+const ErrorResponse = require('../utils/error');
 
-// // @desc    Uploads a picture in claudinary
-// // @route   GET /user/upload
-// // @access  User
-// router.post('/upload', fileUploader.single('imageUrl'), (req,res,next) =>{
-//     if(!req.file){
-//         next(new ErrorResponse('No file uploaded', 500));
-//         return;
-//     }
-//     res.json({fileUrl: req.file.path});
-// });
-// // @desc    gets the logged in user
-// // @route   GET /user/loggedInUser
-// // @access  User
-// router.get('/loggedInUser', isAuthenticated, async (req,res,next) =>{
-//     try {
-//         const user = await User.findById(req.payload._id);
-//         if(!user){
-//             next(new ErrorResponse('No user found', 404));
-//             return;
-//         }
-//         res.status(200).json({data: user})
-//     } catch (error) {
-//         error = new ErrorResponse(message, 400);
-//     }
-// });
-// // @desc    Updates user from the Database
-// // @route   PUT /user/edit
-// // @access  User
-// router.put('/edit', isAuthenticated, async (req,res,next) =>{
-//     const {username, email, biography, imageUrl} = req.body
-//     if(email === '' || username === ''){
-//         return next(new ErrorResponse('Please fill all the fields.', 400))
-//     }
-//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-//     if (!emailRegex.test(email)) {
-//          return next(new ErrorResponse('Email is not a valid format', 400))
-//     }
-//   try {
-//     const user = await User.findById(req.payload._id);
-//     if(!user){
-//         next(new ErrorResponse('No user found', 404));
-//         return;
-//     } else{
-//         const updatedUser = await User.findByIdAndUpdate(req.payload._id, req.body, {new:true});
-//         res.status(200).json({data: updatedUser})
-//     }
-//   } catch (error) {
-//     error = new ErrorResponse(message, 400);
-//   }
-// })
-// // @desc    Deletes user from the Database
-// // @route   DELETE /user/delete
-// // @access  User
-// router.delete('/delete', isAuthenticated, async (req,res,next) =>{
-//     const userId = req.payload;
-//     try {
-//         const deletedUser = await User.findByIdAndDelete(userId);
-//         res.status(202).json({data: deletedUser});
-//     } catch (error) {
-//         error = new ErrorResponse(message, 400);
-//     }
-// });
-// // @desc    Admin can delete any users
-// // @route   DELETE /user/:userId/delete
-// // @access  Admin
-// router.delete('/:userId/delete', isAuthenticated, isAdmin, async (req,res,next) =>{
-//     const {userId} = req.params;
-//     try {
-//         const deletedUser = await User.findByIdAndDelete(userId);
-//         res.status(202).json({data: deletedUser});
-//     } catch (error) {
-//         error = new ErrorResponse(message, 400);
-//     }
-// });
-// // @desc    Shows userlist to Admin
-// // @route   Get /user/userList
-// // @access  Admin
-// router.get('/userList', isAuthenticated,isAdmin, async (req, res, next) => {
-//     try {
-//         const users = await User.find({});
-//         res.status(200).json({ data: users });
-//     } catch (error) {
-//         error = new ErrorResponse(message, 400);
-//     }
-// });
-// // @desc    Allows the user to update its own filters to get personalized recommendations.
-// // @route   PUT /user/preferences
-// // @access  User
-// router.put("/preferences", isAuthenticated, async(req, res, next) => {
-//     const userId = req.payload._id;
-//     const newPreferences = req.body;
-//     try {
-//         await User.findOneAndUpdate({userId:userId}, {preferences: newPreferences});
-//         res.status(202).json({data: newPreferences});
-//     } catch (error) {
-//         error = new ErrorResponse(message, 400);
-//     }
-// });
+// @desc    Uploads a picture in claudinary
+// @route   GET /user/upload
+// @access  User
+router.post('/upload', fileUploader.single('imageUrl'), (req,res,next) =>{
+    if(!req.file){
+        next(new ErrorResponse('No file uploaded', 500));
+        return;
+    }
+    res.json({fileUrl: req.file.path});
+});
+// @desc    gets the logged in user
+// @route   GET /user/loggedInUser
+// @access  User
+router.get('/loggedInUser', isAuthenticated, async (req,res,next) =>{
+    try {
+        const user = await User.findById(req.payload._id);
+        if(!user){
+            next(new ErrorResponse('No user found', 404));
+            return;
+        }
+        res.status(200).json({data: user})
+    } catch (error) {
+        error = new ErrorResponse(message, 400);
+    }
+});
+// @desc    Updates user from the Database
+// @route   PUT /user/edit
+// @access  User
+router.put('/edit', isAuthenticated, async (req,res,next) =>{
+    const {username, email, biography, imageUrl} = req.body
+    if(email === '' || username === ''){
+        return next(new ErrorResponse('Please fill all the fields.', 400))
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    if (!emailRegex.test(email)) {
+         return next(new ErrorResponse('Email is not a valid format', 400))
+    }
+  try {
+    const user = await User.findById(req.payload._id);
+    if(!user){
+        next(new ErrorResponse('No user found', 404));
+        return;
+    } else{
+        const updatedUser = await User.findByIdAndUpdate(req.payload._id, req.body, {new:true});
+        res.status(200).json({data: updatedUser})
+    }
+  } catch (error) {
+    error = new ErrorResponse(message, 400);
+  }
+})
+// @desc    Deletes user from the Database
+// @route   DELETE /user/delete
+// @access  User
+router.delete('/delete', isAuthenticated, async (req,res,next) =>{
+    const userId = req.payload;
+    try {
+        const deletedUser = await User.findByIdAndDelete(userId);
+        res.status(202).json({data: deletedUser});
+    } catch (error) {
+        error = new ErrorResponse(message, 400);
+    }
+});
+// @desc    Admin can delete any users
+// @route   DELETE /user/:userId/delete
+// @access  Admin
+router.delete('/:userId/delete', isAuthenticated, isAdmin, async (req,res,next) =>{
+    const {userId} = req.params;
+    try {
+        const deletedUser = await User.findByIdAndDelete(userId);
+        res.status(202).json({data: deletedUser});
+    } catch (error) {
+        error = new ErrorResponse(message, 400);
+    }
+});
+// @desc    Shows userlist to Admin
+// @route   Get /user/userList
+// @access  Admin
+router.get('/userList', isAuthenticated,isAdmin, async (req, res, next) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json({ data: users });
+    } catch (error) {
+        error = new ErrorResponse(message, 400);
+    }
+});
+// @desc    Allows the user to update its own filters to get personalized recommendations.
+// @route   PUT /user/preferences
+// @access  User
+router.put("/preferences", isAuthenticated, async(req, res, next) => {
+    const userId = req.payload._id;
+    const newPreferences = req.body;
+    try {
+        await User.findOneAndUpdate({userId:userId}, {preferences: newPreferences});
+        res.status(202).json({data: newPreferences});
+    } catch (error) {
+        error = new ErrorResponse(message, 400);
+    }
+});
 
-// module.exports = router;
+module.exports = router;
